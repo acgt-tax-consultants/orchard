45acbcf6021982c4af5b5d3f5da8d5a4584c8f85
Jorden <jakereps@users.noreply.github.com>
2017-02-21 15:43:52 -0700

    ENH: Fix luigi setup to use external task wrapper

--- sample pipeline/module1.c
+++ sample pipeline/module1.c
@@ -7,16 +7,16 @@ void read_file(char* in_filename){
 }
 
 void write_file(char* out_filename, int x){
-    FILE *out_file = fopen(out_filename, "w");
+    FILE *out_file = fopen(out_filename, "wb");
     fprintf(out_file, "This is the output of test module 1.\n");
     fprintf(out_file, "%d", x);
 }
 
 int main(int argc, char** argv) {
 
-  if (argc != 3){
+  if (argc != 4){
         printf("usage: module1 inputfilename outputfilename int");
-        return(1);
+        return(-1);
     }
 
     int temp = atoi(argv[3]);
